7700982d46b7a0874d5b59ff111af5f109d5041e
nguyening20 <36948494+nguyening20@users.noreply.github.com>
2020-03-20 15:38:29 -0400

    client build

---
+++ client/build/precache-manifest.9691383e4af8fb38ef8cc8c6981449d8.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ef2bce7691032618a3697505adb7dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a401efbf79bd0b2d6bad",
+    "url": "/static/css/main.edce2c23.chunk.css"
+  },
+  {
+    "revision": "e7de783bc27653a582fc",
+    "url": "/static/js/2.ff47c3f9.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.ff47c3f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a401efbf79bd0b2d6bad",
+    "url": "/static/js/main.cbf64472.chunk.js"
+  },
+  {
+    "revision": "ad98e786d1539fbc4120",
+    "url": "/static/js/runtime-main.b225118f.js"
+  },
+  {
+    "revision": "b5714b6acfbff7904a24ea78662b9345",
+    "url": "/static/media/100_1.b5714b6a.jpg"
+  },
+  {
+    "revision": "9b698d6dbfaf10d84d983440e2b5a433",
+    "url": "/static/media/100_2.9b698d6d.jpg"
+  },
+  {
+    "revision": "b618b5fc19f0517b8428bc9cae806df0",
+    "url": "/static/media/101_1.b618b5fc.jpg"
+  },
+  {
+    "revision": "2e62289b2a88630ff3461c81af1756f7",
+    "url": "/static/media/10412368723880252_1.2e62289b.jpg"
+  },
+  {
+    "revision": "54fbf816a3acf53727a2536d97d76881",
+    "url": "/static/media/10547961582846888_1.54fbf816.jpg"
+  },
+  {
+    "revision": "0d941b4cc1da99273f35b2777b500ff3",
+    "url": "/static/media/10686354557628304_1.0d941b4c.jpg"
+  },
+  {
+    "revision": "84c82a5a8f51a94e64024f67e8445d37",
+    "url": "/static/media/11033926921508488_1.84c82a5a.jpg"
+  },
+  {
+    "revision": "06ec2c628b4a51a539881f0ee02b4e60",
+    "url": "/static/media/11600983276356164_1.06ec2c62.jpg"
+  },
+  {
+    "revision": "7faf6a31b8557166976235d3bd9a8e2b",
+    "url": "/static/media/11854078013954528_1.7faf6a31.jpg"
+  },
+  {
+    "revision": "2995d79a12f2b95cd2315d66c5bb5084",
+    "url": "/static/media/12064273040195392_1.2995d79a.jpg"
+  },
+  {
+    "revision": "d78e27906d034c3b4cd53aa3526acda3",
+    "url": "/static/media/18532669286405344_1.d78e2790.jpg"
+  },
+  {
+    "revision": "75f51cecfca671162c53bdeed3cf549f",
+    "url": "/static/media/18644119330491310_1.75f51cec.jpg"
+  },
+  {
+    "revision": "4c799bac69deb57ceba00bcdf15e6ec2",
+    "url": "/static/media/27250082398145996_1.4c799bac.jpg"
+  },
+  {
+    "revision": "a313534ce6b430f29143cafee1eb5a3e",
+    "url": "/static/media/39876704341265610_1.a313534c.jpg"
+  },
+  {
+    "revision": "df947f142806310944c26d76bf1b8fa0",
+    "url": "/static/media/51498472915966370_1.df947f14.jpg"
+  },
+  {
+    "revision": "843a471381aada57cdd38f66662ab1af",
+    "url": "/static/media/5619496040738316_1.843a4713.jpg"
+  },
+  {
+    "revision": "45a051e062934c657ca67819884cbf23",
+    "url": "/static/media/6090484789343891_1.45a051e0.jpg"
+  },
+  {
+    "revision": "87b5353e208124cb9acc7cadd973a3bb",
+    "url": "/static/media/8552515751438644_1.87b5353e.jpg"
+  },
+  {
+    "revision": "df97d7c2d0fc5e5d9346b660ce3936fa",
+    "url": "/static/media/876661122392077_1.df97d7c2.jpg"
+  },
+  {
+    "revision": "59b643658d74a090ed326573a46e07a7",
+    "url": "/static/media/9197907543445676_1.59b64365.jpg"
+  },
+  {
+    "revision": "fc4a4031b9ee7acba7939e1204d99364",
+    "url": "/static/media/Apple.fc4a4031.png"
+  },
+  {
+    "revision": "406224a92ece6629e15fa70ea6cf4cbd",
+    "url": "/static/media/Christian.406224a9.jpg"
+  },
+  {
+    "revision": "fba831bb1c3ce1009f125bdbead8ca29",
+    "url": "/static/media/CoorsLight.fba831bb.jpg"
+  },
+  {
+    "revision": "5d22bbbf8b8a9b58704ba985abb0db13",
+    "url": "/static/media/Shane.5d22bbbf.jpg"
+  },
+  {
+    "revision": "69d6bcd7e0f72b86c4ca13b65fe619ff",
+    "url": "/static/media/Spotify.69d6bcd7.jpg"
+  },
+  {
+    "revision": "45bc6a09ca1a22e8534b7980ede73824",
+    "url": "/static/media/anchor.45bc6a09.png"
+  },
+  {
+    "revision": "7d65a2eacc249663292e2336e88940e2",
+    "url": "/static/media/instagram-icon.7d65a2ea.png"
+  },
+  {
+    "revision": "3d787c7393175edcbe6faa7e55ed6b7c",
+    "url": "/static/media/logo.3d787c73.png"
+  },
+  {
+    "revision": "821712fcc6222b2f97eb0aae43442c08",
+    "url": "/static/media/studio.821712fc.jpg"
+  }
+]);

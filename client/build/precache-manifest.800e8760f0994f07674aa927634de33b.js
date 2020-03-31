4d860ece780fd5a29658288e077dce8c72c66dc6
nguyening20 <36948494+nguyening20@users.noreply.github.com>
2020-03-31 19:23:48 -0400

    Revert "Revert "Improve Load Performance""
    This reverts commit b489a38d7cc68d9cc0e5eaa25cf52dddb46283e4.

---
+++ client/build/precache-manifest.800e8760f0994f07674aa927634de33b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26024168cf7b248aec984025dae819f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "839a5d50a224baba7140",
+    "url": "/static/css/5.f05f5a63.chunk.css"
+  },
+  {
+    "revision": "c382a552befa2c165282",
+    "url": "/static/css/main.aa200aa8.chunk.css"
+  },
+  {
+    "revision": "6ab3d669b7eca777af46",
+    "url": "/static/js/2.2331b200.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.2331b200.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bfb6eb66a39aa059889",
+    "url": "/static/js/3.8b84911e.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/3.8b84911e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4cb9ced2a32eb99ad310",
+    "url": "/static/js/4.1a671f68.chunk.js"
+  },
+  {
+    "revision": "839a5d50a224baba7140",
+    "url": "/static/js/5.35fd31f4.chunk.js"
+  },
+  {
+    "revision": "2063776ff1d752faaf3d",
+    "url": "/static/js/6.2c23d2e9.chunk.js"
+  },
+  {
+    "revision": "c382a552befa2c165282",
+    "url": "/static/js/main.1b872372.chunk.js"
+  },
+  {
+    "revision": "3e1e910639e5e46cb670",
+    "url": "/static/js/runtime-main.51ef85c5.js"
+  },
+  {
+    "revision": "fc4a4031b9ee7acba7939e1204d99364",
+    "url": "/static/media/Apple.fc4a4031.png"
+  },
+  {
+    "revision": "406224a92ece6629e15fa70ea6cf4cbd",
+    "url": "/static/media/Christian.406224a9.jpg"
+  },
+  {
+    "revision": "fba831bb1c3ce1009f125bdbead8ca29",
+    "url": "/static/media/CoorsLight.fba831bb.jpg"
+  },
+  {
+    "revision": "5d22bbbf8b8a9b58704ba985abb0db13",
+    "url": "/static/media/Shane.5d22bbbf.jpg"
+  },
+  {
+    "revision": "69d6bcd7e0f72b86c4ca13b65fe619ff",
+    "url": "/static/media/Spotify.69d6bcd7.jpg"
+  },
+  {
+    "revision": "45bc6a09ca1a22e8534b7980ede73824",
+    "url": "/static/media/anchor.45bc6a09.png"
+  },
+  {
+    "revision": "3d787c7393175edcbe6faa7e55ed6b7c",
+    "url": "/static/media/logo.3d787c73.png"
+  },
+  {
+    "revision": "d7580d39dfa30ab3ad49d4c50c1a9a25",
+    "url": "/static/media/studio.d7580d39.jpg"
+  }
+]);
--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
 import Team from './components/Team';
 import PodcastList from './components/PodcastList';
 import CoolStuff from './components/CoolStuff';
@@ -8,32 +8,32 @@ const scrollToRef = (ref) => window.scrollTo({
   left: 0,
   top: ref.current.offsetTop,
   behavior: 'smooth'
-}) ;
+});
 
 const Home = () => {
   const myRef = useRef(null);
 
   return (
     <>
-    <div className="row align-items-center vh-100">
-    <div className="col align-self-center">
-      <h1 className="display-4 font-weight-normal">Books & Brewskies</h1>
-      <p className="lead font-weight-normal">Created by the men, for the boys.</p>
-      <iframe title="podcast-1" className="mb-4 w-auto" src="https://anchor.fm/books--brewskies/embed/episodes/Episode-9-Markus-Miller-Quarantine-Life-ebpu9t/a-a1bvsum" height="102px" width="400px" frameBorder="0" scrolling="no"></iframe>
-      <div onClick={()=>scrollToRef(myRef)}>
-      <svg className="bi bi-chevron-down" width="6em" height="6em" viewBox="0 0 20 20" fill="white" xmlns="http://www.w3.org/2000/svg">
-        <path fillRule="evenodd" d="M3.646 6.646a.5.5 0 01.708 0L10 12.293l5.646-5.647a.5.5 0 01.708.708l-6 6a.5.5 0 01-.708 0l-6-6a.5.5 0 010-.708z" clipRule="evenodd"></path>
-      </svg>
+      <div className="row align-items-center vh-100">
+        <div className="col align-self-center">
+          <h1 className="display-4 font-weight-normal">Books & Brewskies</h1>
+          <p className="lead font-weight-normal">Created by the men, for the boys.</p>
+          <iframe title="podcast-1" className="mb-4 w-auto" src="https://anchor.fm/books--brewskies/embed/episodes/Episode-9-Markus-Miller-Quarantine-Life-ebpu9t/a-a1bvsum" height="102px" width="400px" frameBorder="0" scrolling="no"></iframe>
+          <div onClick={() => scrollToRef(myRef)}>
+            <svg className="bi bi-chevron-down" width="6em" height="6em" viewBox="0 0 20 20" fill="white" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M3.646 6.646a.5.5 0 01.708 0L10 12.293l5.646-5.647a.5.5 0 01.708.708l-6 6a.5.5 0 01-.708 0l-6-6a.5.5 0 010-.708z" clipRule="evenodd"></path>
+            </svg>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-    <div ref={myRef} style={{height: "20px"}} />
-    <div className="container home-context rounded">
-      <Team />
-      <PodcastList />
-      <Platforms />
-      <CoolStuff />
-    </div>
+      <div ref={myRef} style={{ height: "20px" }} />
+      <div className="container home-context rounded">
+        <Team />
+        <PodcastList />
+        <Platforms />
+        <CoolStuff />
+      </div>
     </>
   )
 }

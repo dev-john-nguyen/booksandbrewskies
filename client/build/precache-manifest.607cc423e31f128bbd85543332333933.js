5c565fbce6a89e83a4af073c3ec999daba412528
nguyening20 <36948494+nguyening20@users.noreply.github.com>
2020-04-06 21:22:57 -0400

    Production Build
    Production Build (side note - future update social links)

---
+++ client/build/precache-manifest.607cc423e31f128bbd85543332333933.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ad770168ebd99b9d2b8f2296c47518",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1f3b945dc6b419650de",
+    "url": "/static/css/4.044a2183.chunk.css"
+  },
+  {
+    "revision": "dcdc0f2c14ac54c8cefe",
+    "url": "/static/css/main.4b483269.chunk.css"
+  },
+  {
+    "revision": "8a01e0949eb7b99045c7",
+    "url": "/static/js/10.64847209.chunk.js"
+  },
+  {
+    "revision": "cfe1ed6d5ea5accbe32e",
+    "url": "/static/js/11.6295d98b.chunk.js"
+  },
+  {
+    "revision": "e56cee9afe8b7ab88490",
+    "url": "/static/js/2.84378173.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.84378173.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c95c85bd487d44c07ed4",
+    "url": "/static/js/3.a79e45a3.chunk.js"
+  },
+  {
+    "revision": "c1f3b945dc6b419650de",
+    "url": "/static/js/4.7594350e.chunk.js"
+  },
+  {
+    "revision": "ceae1ea914a0c077a4a8",
+    "url": "/static/js/5.f04569bd.chunk.js"
+  },
+  {
+    "revision": "69fdcaea5d6517c9d10f",
+    "url": "/static/js/6.809ce26f.chunk.js"
+  },
+  {
+    "revision": "02b4ee762f92a1f0b5d9",
+    "url": "/static/js/7.efc8ba71.chunk.js"
+  },
+  {
+    "revision": "1db14eda3c6bc7641544",
+    "url": "/static/js/8.2261403d.chunk.js"
+  },
+  {
+    "revision": "533576e26f72420d729f",
+    "url": "/static/js/9.00daa159.chunk.js"
+  },
+  {
+    "revision": "dcdc0f2c14ac54c8cefe",
+    "url": "/static/js/main.9d04a844.chunk.js"
+  },
+  {
+    "revision": "046bcc15739d278cc681",
+    "url": "/static/js/runtime-main.ed494afa.js"
+  },
+  {
+    "revision": "d376a881c46136294ef08651f12c61df",
+    "url": "/static/media/10547961582846888_1.d376a881.jpg"
+  },
+  {
+    "revision": "d376a881c46136294ef08651f12c61df",
+    "url": "/static/media/11854078013954528_1.d376a881.jpg"
+  },
+  {
+    "revision": "d376a881c46136294ef08651f12c61df",
+    "url": "/static/media/18532669286405344_1.d376a881.jpg"
+  },
+  {
+    "revision": "d376a881c46136294ef08651f12c61df",
+    "url": "/static/media/18644119330491310_1.d376a881.jpg"
+  },
+  {
+    "revision": "d376a881c46136294ef08651f12c61df",
+    "url": "/static/media/5619496040738316_1.d376a881.jpg"
+  },
+  {
+    "revision": "d376a881c46136294ef08651f12c61df",
+    "url": "/static/media/6090484789343891_1.d376a881.jpg"
+  },
+  {
+    "revision": "d376a881c46136294ef08651f12c61df",
+    "url": "/static/media/8552515751438644_1.d376a881.jpg"
+  },
+  {
+    "revision": "d376a881c46136294ef08651f12c61df",
+    "url": "/static/media/876661122392077_1.d376a881.jpg"
+  },
+  {
+    "revision": "d376a881c46136294ef08651f12c61df",
+    "url": "/static/media/9197907543445676_1.d376a881.jpg"
+  },
+  {
+    "revision": "1675c8428476f9435b8ab56ca2f6031a",
+    "url": "/static/media/Christian.1675c842.png"
+  },
+  {
+    "revision": "2e939365b3884ac0ab92b2a2db5ddc0a",
+    "url": "/static/media/CoorsLight.2e939365.jpg"
+  },
+  {
+    "revision": "34655466f9057d38160408e0d51a4f82",
+    "url": "/static/media/Image 5.34655466.svg"
+  },
+  {
+    "revision": "b995789a14342849ff4e163a45c11cbc",
+    "url": "/static/media/John-Nguyen.b995789a.png"
+  },
+  {
+    "revision": "c8311fa3d9aae43df8aa292bdedb014a",
+    "url": "/static/media/Shane.c8311fa3.png"
+  },
+  {
+    "revision": "3d787c7393175edcbe6faa7e55ed6b7c",
+    "url": "/static/media/logo.3d787c73.png"
+  },
+  {
+    "revision": "cf4f7d49ecfa1398d821028b4b1c2a52",
+    "url": "/static/media/logo_products.cf4f7d49.png"
+  },
+  {
+    "revision": "d7580d39dfa30ab3ad49d4c50c1a9a25",
+    "url": "/static/media/studio.d7580d39.jpg"
+  }
+]);
